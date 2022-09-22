c6157d0eabbf1f58bae4e7eb6534b4d9ad968f35
lidu-1 <lilijoli65@gmail.com>
2022-09-23 01:58:01 +0300

    rectangular

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,12 +1,6 @@
-guillaume@ubuntu:~/0x13$ cat 0-main.js
 #!/usr/bin/node
 const Rectangle = require('./0-rectangle');
 
 const r1 = new Rectangle();
 console.log(r1);
 console.log(r1.constructor);
-
-guillaume@ubuntu:~/0x13$ ./0-main.js
-Rectangle {}
-[Function: Rectangle]
-guillaume@ubuntu:~/0x13$ 
