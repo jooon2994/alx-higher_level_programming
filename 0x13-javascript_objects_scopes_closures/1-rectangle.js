7576e7ddd05639dbe4f249260410600a21c139e9
lidu-1 <lilijoli65@gmail.com>
2022-09-22 23:11:46 +0300

    rectangle-1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,30 +1,9 @@
-guillaume@ubuntu:~/0x13$ cat 1-main.js
 #!/usr/bin/node
-const Rectangle = require('./1-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-
-guillaume@ubuntu:~/0x13$ ./1-main.js
-Rectangle { width: 2, height: 3 }
-2
-3
-Rectangle { width: 2, height: -3 }
-2
--3
-Rectangle { width: 2, height: undefined }
-2
-undefined
-guillaume@ubuntu:~/0x13$ 
+module.exports = Rectangle;
