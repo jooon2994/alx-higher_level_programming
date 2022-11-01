469f300cf6ee8a1d2145600512735f0b34683666
jooon2994 <wizyoni7@gmail.com>
2022-11-01 11:43:16 +0300

    initial commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
+#sends a request to url and display the size of the respose
+curl -so /dev/null "$1" -w '%{size_download}\n'
